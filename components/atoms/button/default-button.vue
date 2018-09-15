874fb4ea6fbbc2ee49276e8f756ce1679ec1067e
yagisuke <yagisuke.0209@gmail.com>
2018-09-16 00:27:07 +0900

    addStorybook

--- components/atoms/button/default-button.vue
+++ components/atoms/button/default-button.vue
@@ -1,7 +1,20 @@
 <template>
-  <button type="button"><slot /></button>
+  <button class="default-button" type="button"><slot /></button>
 </template>
 
-<script>export default {}</script>
+<script>
+export default {
+}
+</script>
 
-<style></style>
+<style lang="scss" scoped>
+.default-button {
+  width: 240px;
+  height: 80px;
+  font-size: 32px;
+  border: 1px solid gray;
+  border-radius: 8px;
+  background-color: white;
+  color: gray;
+}
+</style>
